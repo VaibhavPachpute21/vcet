ee97f40c16b0b7afd30e393f700121f6a9e18bbe
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-10-08 11:50:42 +0530

    whois

--- scan.py
+++ scan.py
@@ -4,5 +4,15 @@ import whois
 def printIp (domain) :
     ip_address = socket.gethostbyname(domain)
     sayIp = f'Domain Name:{domain}\nDomain IP:{ip_address}'
+
     w=whois.whois(domain)
-    return sayIp+str(w)
+    Registerar=str(w['registrar'])
+    Creation=str(w['creation_date'])
+    Expiration=str(w['expiration_date'])
+    Servers=str(w['name_servers'])
+    Country=str(w['country'])
+    Emails=str(w['emails'])
+
+    basicInfo=f'\nRegisterar:{Registerar}\nCreation Date:{Creation}\nExpiration Date:{Expiration}\nName Server:{Servers}\nCountry:{Country}\nEmails:{Emails}'
+
+    return sayIp+basicInfo
